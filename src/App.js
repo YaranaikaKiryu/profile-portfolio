a2bb4ce59ec9dd77bc540f004c08b43b17b10ceb
YaranaikaKiryu <146524104+YaranaikaKiryu@users.noreply.github.com>
2024-02-22 17:56:31 +0800

    COmmit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from "react";
-import { Image, Text, VStack, Heading, Button, Box,  Stack, HStack, SimpleGrid,Divider, StackDivider,ButtonGroup, Container, Input } from "@chakra-ui/react";
+import { Image, Text, VStack, Heading, Button, Box,  Stack, HStack, SimpleGrid,Divider, StackDivider, Container, Link} from "@chakra-ui/react";
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import { Tag } from '@chakra-ui/react'
 import { Flex } from "@chakra-ui/react";
-import { Grid, GridItem } from '@chakra-ui/react'
+import {useColorMode} from "@chakra-ui/react"
+
 
 function Profile() {
   return (
@@ -14,20 +16,18 @@ function Profile() {
     backgroundSize="cover"
    color= "white"
   >
-
       <VStack spacing={8} align="center">
         <Image
           mt="20"
           borderRadius="half"
-          boxSize="200px"
-          src="https://scontent.fcrk2-1.fna.fbcdn.net/v/t1.18169-9/21740321_1407186486067287_2409406893115347629_n.png?_nc_cat=108&ccb=1-7&_nc_sid=7f8c78&_nc_eui2=AeHV2nr-O9UxN6IQZ5IaBPs402knAQzTCsDTaScBDNMKwDfOvQ78ZWGMPdhHcVOJiEZLF2Pix8ibdxiHNnzFCkob&_nc_ohc=-X_3QkZCQ_IAX8xOuo3&_nc_ht=scontent.fcrk2-1.fna&oh=00_AfAycvhcPIdQ8di0NITa-9EBWgGGCW0sOcKXdhyIvQ2PCQ&oe=65EF0C7F"
+          boxSize="220px"
+          src="https://i2-prod.mirror.co.uk/incoming/article29490204.ece/ALTERNATES/s1200c/0_Top-pic-template-copy.jpg"
           alt="M16A1"
-        
         />
         <Heading as="h2" size="xl">
-      [PLACE HOLDER]
+      Mark Louis C. Cadiente
         </Heading>
-        <Text>Mental Breakdown Specialist</Text>
+        <Text>First Year College Technical Service Management</Text>
         <Text>
         What did the ocean said to the beach? Nothing it just waved. Want some Ginger Ale?
         </Text>
@@ -68,19 +68,22 @@ function Profile() {
 }
 
 function Contents() {
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
+    
     <Container maxW='8xl' bg='white.600'>
     <Flex direction={{ base: 'column', lg: 'row' }} justifyContent='space-between'>
-      <Card maxW='lg' marginTop='4' boxShadow='2xl' bg='baby powder.600' height='230px'>
+    <Card maxW='lg' marginTop='4' boxShadow='2xl' bg='baby powder.600' height='250px'>
   <CardHeader>
-    <Heading size='lg'>About</Heading>
+    <Heading size='lg'>About Mark</Heading>
   </CardHeader>
   <CardBody height='200px' overflow='auto'>
-            <Stack divider={<StackDivider />} spacing='1'>
-              <Box>
-                <Text pt='1' fontSize='sm'>
-                  Quasars, the single most powerful objects in existence - so powerful that they can devour a galaxy.
-                </Text >
+    <Stack divider={<StackDivider/>} spacing='1'>
+      <Box>
+        <Text pt='0' fontSize='sm'>
+          I am Yaranaika Kiryu, a Mental Breakdown Specialist. I am a fan of technology, music, and anime. I’m also interested in gaming and studying.
+          I am currently Studying Python and Java for my chosen Course. I am also a collector of rare items and a traveler.
+        </Text>
                 <HStack spacing={5} mt={3}>
                   {['sm'].map((size) => ( 
                     <Tag size={size} key={size} variant='solid' colorScheme='teal'>
@@ -113,14 +116,127 @@ function Contents() {
                     </Tag>
                   ))}
                 </HStack>
+                
               </Box>
             </Stack>
           </CardBody>
         </Card>
 
-        <Box display={{ base: 'none', lg: 'block' }} width='26px' />
 
+
+
+
+        <Box display={{ base: 'none', lg: 'block' }} width='26px'/>
         <Card marginTop='4' direction={{ base: 'column', sm: 'row' }} overflow='hidden' variant='outline' maxW="850px" height='250px' boxShadow='2xl'>
+  <Stack>
+    <CardBody height='200px' overflow='auto'>
+      <Heading size='md'>Support Mark Louis Cadiente</Heading>
+      <Text py='2'>
+      Your support is greatly appreciated! Your contributions help keep our work going and make a huge difference. 
+      Whether it's a one-time donation or a recurring contribution, every bit helps. 
+      Thank you for being a part of our community and for your generous support.
+      </Text>
+    </CardBody>
+    <CardFooter>
+      
+    <Button onClick={toggleColorMode}>
+        Click Here {colorMode === "light"}
+      </Button>
+    </CardFooter>
+  </Stack>
+</Card>
+</Flex>
+
+<Flex direction={{ base: 'column', lg: 'row' }} justifyContent='space-between'>
+      <Flex justifyContent='flex-start'>
+        <Card maxW='lg' marginTop='4' boxShadow='2xl'bg='baby powder.600' height='640px'>
+          <CardHeader>
+            <Heading size='lg'>Gallery</Heading>
+          </CardHeader>
+          <CardBody>
+          <SimpleGrid columns={3} spacing={5}>
+  <Box  height='160px'>
+    <Image src="https://scontent.fmnl4-3.fna.fbcdn.net/v/t39.30808-6/358681464_1303232810312225_1908921387589452808_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHvUL8PynTcrkxdKdLI8JB92cSXwTxOLGzZxJfBPE4sbLqhlKdrgDJJ0CeACAxP4GQrqVNRvvieLezfHN2FiJyf&_nc_ohc=SePls4RiLkMAX_EPWlc&_nc_ht=scontent.fmnl4-3.fna&oh=00_AfDJVSJVlYHL4DSZQpY6GCqnWj0NEOgcr3DPulG7Iqa-Gg&oe=65DC4130" alt="Sample image 1" boxSize="150px" />
+  </Box>
+  <Box  height='160px'>
+    <Image src="https://media.discordapp.net/attachments/687923095159308426/1168735097697091737/20231031_101550.jpg?ex=65e68061&is=65d40b61&hm=4dab838f9b011de8fcd1fec958950c5123b7352a0a6787cdb5d5a5f560b2eec4&=&format=webp&width=313&height=676" alt="Sample image 2" boxSize="150px" />
+  </Box>
+  <Box  height='160px'>
+    <Image src="https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=680" alt="Sample image 3" boxSize="150px" />
+  </Box>
+  <Box  height='160px'>
+    <Image src="https://cdn.discordapp.com/attachments/687923095159308426/1209841291370831892/280bed666ef01880e9fd2423f6227a1f_4315930060208472198.png?ex=65e86381&is=65d5ee81&hm=dc5a9fe144f8f6ff44eedcbd5df62f16434cb4aec2c03dbc6c0464344fc8b704&" alt="Sample image 4" boxSize="150px" />
+  </Box>
+  <Box  height='160px'>
+    <Image src="https://preview.redd.it/exactly-17-hours-ago-ichiban-kasuga-turned-46-years-old-v0-sd0p16frjf9a1.png?auto=webp&s=860c482da9428f011d591d59df7b255c4ec3b915" alt="Sample image 5" boxSize="150px" />
+  </Box>
+  <Box  height='160px'>
+    <Image src="https://media.discordapp.net/attachments/687923095159308426/1207931809053282397/fkgxLXZiWzlSJmZe_zMH4EzWM2sN0hvjfMcQffh6L-bO8csikzvqBCb8G4Cjq1qiPwIiGTfod4RPVgs640-nd-v1.png?ex=65e17129&is=65cefc29&hm=5a52bbee76126ba4f5c4e24d757423b84c18cde1c98d09188505137b3e16c555&=&format=webp&quality=lossless" alt="Sample image 6" boxSize="150px" />
+  </Box>
+  <Box  height='160px'>
+    <Image src="https://media.discordapp.net/attachments/687923095159308426/1161931651568373831/20231012_154117.jpg?ex=65e0352d&is=65cdc02d&hm=e85c146f74b696921bc65682d7453494031411ae72c47dbd7644ea9f85a7892f&=&format=webp&width=313&height=676" alt="Sample image 7" boxSize="150px" />
+  </Box>
+  <Box  height='160px'>
+    <Image src="https://media.discordapp.net/attachments/687923095159308426/1163164255722422312/image.png?ex=65e4b121&is=65d23c21&hm=0ae3e925a2967db0badf90233af13ae16d9200e63ec0a5fcd4c86b37d89b1fbf&=&format=webp&quality=lossless&width=898&height=676" alt="Sample image 8" boxSize="150px" />
+  </Box>
+  <Box  height='160px'>
+    <Image src="https://media.discordapp.net/attachments/687923095159308426/1209336141476663356/rn_image_picker_lib_temp_691e94c4-d810-4b03-bd41-9f850505f993.jpg?ex=65e68d0c&is=65d4180c&hm=cd01bb47ec8849176db3580b84abfce6cc87f48db5f0cbd880468e2e37bcb774&=&format=webp&width=1440&height=666" alt="Sample image 9" boxSize="150px" />
+  </Box>
+</SimpleGrid>
+          </CardBody>
+        </Card>
+      </Flex>
+
+<Card marginTop='4' direction={{ base: 'column', sm: 'row' }}maxW="850px" height='1590px' backgroundColor='transparent' outline='none' boxShadow='none'>
+
+  <Stack>
+    <CardBody height='200px' overflow='auto'>
+        <Card marginTop='4' direction={{ base: 'column', sm: 'row' }} overflow='hidden' variant='outline' maxW="850px" height='500px' boxShadow='2xl'>
+  <Stack>
+    <CardBody height='200px' overflow='auto'>
+      <Heading size='lg'>More About Mark</Heading>
+      <Text py='2'>
+        Currently Studying at Tarlac State University San Isidro Extension Campus, Currently taking up Bachelor of Science in 
+        Information Technology Major in Technical Service Management. Also Currently Studying Python and Java for my chosen Course.
+        As of now I am currently a 1st Year Student. So I am still new at some areas of my course.
+        <CardHeader>
+    <Heading size='md'>Links</Heading>
+  </CardHeader>
+
+  <CardBody>
+    <Stack divider={<StackDivider />} spacing='4'>
+      <Box>
+        <Heading size='xs' textTransform='uppercase'>
+          Github
+        </Heading>
+    
+        <Link> https://github.com/YaranaikaKiryu <ExternalLinkIcon mx="2px" /></Link>
+      
+      </Box>
+      <Box>
+        <Heading size='xs' textTransform='uppercase'>
+          Youtube
+        </Heading>
+        <Link>
+          https://www.youtube.com/@markiplier
+          <ExternalLinkIcon mx="2px" /></Link>
+      </Box>
+      <Box>
+        <Heading size='xs' textTransform='uppercase'>
+          Facebook
+        </Heading>
+        <Link>
+        https://www.facebook.com/marklouis.cadiente
+        <ExternalLinkIcon mx="2px" /></Link>
+      </Box>
+    </Stack>
+  </CardBody>
+      </Text>
+    </CardBody>
+  </Stack>
+</Card>
+
+<Card marginTop='4' direction={{ base: 'column', sm: 'row' }} overflow='hidden' variant='outline' maxW="850px" height='680px' boxShadow='2xl'>
   <Stack>
     <CardBody height='200px' overflow='auto'>
       <Heading size='md'>Support Yaranaika Kiryu</Heading>
@@ -128,63 +244,34 @@ function Contents() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        
       </Text>
+      <Image
+    objectFit='cover'
+    src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+    alt='Chakra UI'
+  />
     </CardBody>
-    <CardFooter>
-      <Button variant='solid' colorScheme='blue'>
-        Buy Latte
-      </Button>
-    </CardFooter>
   </Stack>
 </Card>
 
-</Flex>
 
 
 
 
-      <Flex justifyContent='flex-start'>
-        <Card maxW='lg' marginTop='4' boxShadow='2xl'bg='baby powder.600'>
-          <CardHeader>
-            <Heading size='lg'>Gallery</Heading>
-          </CardHeader>
-          <CardBody>
-          <SimpleGrid columns={3} spacing={5}>
-  <Box  height='160px'>
-    <Image src="https://via.placeholder.com/150" alt="Sample image 1" boxSize="150px" />
-  </Box>
-  <Box  height='160px'>
-    <Image src="https://via.placeholder.com/150" alt="Sample image 2" boxSize="150px" />
-  </Box>
-  <Box  height='160px'>
-    <Image src="https://via.placeholder.com/150" alt="Sample image 3" boxSize="150px" />
-  </Box>
-  <Box  height='160px'>
-    <Image src="https://via.placeholder.com/150" alt="Sample image 4" boxSize="150px" />
-  </Box>
-  <Box  height='160px'>
-    <Image src="https://via.placeholder.com/150" alt="Sample image 5" boxSize="150px" />
-  </Box>
-  <Box  height='160px'>
-    <Image src="https://via.placeholder.com/150" alt="Sample image 6" boxSize="150px" />
-  </Box>
-  <Box  height='160px'>
-    <Image src="https://via.placeholder.com/150" alt="Sample image 7" boxSize="150px" />
-  </Box>
-  <Box  height='160px'>
-    <Image src="https://via.placeholder.com/150" alt="Sample image 8" boxSize="150px" />
-  </Box>
-  <Box  height='160px'>
-    <Image src="https://via.placeholder.com/150" alt="Sample image 9" boxSize="150px" />
-  </Box>
-</SimpleGrid>
-          </CardBody>
-        </Card>
+
+
+
+
+
+
+  
+    </CardBody>
+  </Stack>
+</Card>
+
       </Flex>
-      
     </Container>
-        
+         
       );
     }
 
